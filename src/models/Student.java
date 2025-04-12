ca2fcd0fd80442855d6878f1998de21ac029b806
Clau <klau2015.cc@gmail.com>
2025-04-12 21:04:09 +0300

    Primul Commit

---
+++ src/models/Student.java
@@ -0,0 +1,41 @@
+package models;
+
+public final class Student extends Utilizator{
+    private int An_Studiu;
+    private int grupa;
+    private int Nr_Matricol;
+    private int nr_rez_disponibil = 3;
+
+    public Student(String nume, String email, Facultate facultate, int AnStudiu, int grupa, int nr_matricol) {
+        super(nume, email, facultate);
+        this.An_Studiu = AnStudiu;
+        this.grupa = grupa;
+        this.Nr_Matricol = nr_matricol;
+    }
+
+
+    public int getAnStudiu() {
+        return An_Studiu;
+    }
+
+    public void setAnStudiu(int anStudiu) {
+        this.An_Studiu = anStudiu;
+    }
+
+    public void solicitaRezervare(Sala sala, int zi, String interval) {
+        Rezervare rezervare = new Rezervare(sala, this, zi, interval);
+        adaugaRezervare(rezervare, sala);
+        System.out.println("Rezervare solicitata pentru sala " + sala.getNume());
+        nr_rez_disponibil = nr_rez_disponibil - 1;
+    }
+
+    @Override
+    public void afisareProfil() {
+        super.afisareProfil();
+        System.out.println("An studiu: " + An_Studiu);
+        System.out.println("Grupa: " + grupa);
+        System.out.println("Numar Matricol: " + Nr_Matricol);
+        System.out.println("Mai ai la dispozitie " + nr_rez_disponibil + " rezervari acesta luna.");
+
+    }
+}
